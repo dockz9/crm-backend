832e91d2c1d83d64485340035d305ae8bd8aad35
dockz9 <zach@stoneparkllc.com>
2026-04-04 12:50:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,4 +75,4 @@ app.get("/contacts/search", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.env(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
