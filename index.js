83a19d31b8336641e2eb48ca0c9a24f4f27cf236
dockz9 <zach@stoneparkllc.com>
2026-04-08 17:14:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
 const API_KEY = process.env.FIREBASE_API_KEY;
